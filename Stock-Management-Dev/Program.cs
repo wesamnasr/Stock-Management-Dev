569631fd307b9733241947a5b12879e739e64dd2
Wesam Nasr Ahmed <wesamnasr999@gmail.com>
2025-06-02 08:49:23 +0300

    delete cutomer pill form

--- Stock-Management-Dev/Program.cs
+++ Stock-Management-Dev/Program.cs
@@ -19,6 +19,7 @@ namespace Stock_Management_Dev
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+
         }
     }
 }
